992ffa4fc93f71405491a2f8a75b2a16fe8f43a7
syekotwa <syekotwa@publicisgroupe.net>
2018-09-04 17:43:57 +0530

    practise session on datatypes and dictionaries

--- BasicExamples/datatypes.py
+++ BasicExamples/datatypes.py
@@ -4,24 +4,31 @@ can be written like this'''
 
 num=[1,2,3,4,5]
 y=3 in num #check 3 is present in num list
-print(y)
+z=45 in num
+if(y):
+    print("y value present")
+if(z):
+    print("z value present")
+else:
+    print("z value not present")
+print("Y value : ",y)
 x= 6 in num #check 6 is present un num list
-print(x)
+print("X value : ",x)
 
 #numbers examples
-print(5**3)
-print(32//3)
+print("Value of 5**3 : ",5**3)
+print("Value of 32//3 : ",32//3)
 
 
 #integer
 a = 10
 #complex data type
 b = 2+8j
-print(b)
+print("value of b : ",b)
 
 #Float data type
 c = 2.35
-print(c)
+print("Value of c : ",c)
 
 '''
 Type convertion
--- BasicExamples/dictionaries.py
+++ BasicExamples/dictionaries.py
@@ -1,29 +1,36 @@
 #DICTIONARIES EXAMPLES
 westeros={'cersei':'lannister','daenerys':'targaeyn','sansa':'stark'}
-print(westeros)
+print("\nDisplaying dictionary keys:values : ",westeros,"\n")
+
 #if you want to change the certain value of a particular key in dictionary
 westeros['daenerys']='dothraki'
-print(westeros)
+print("Dictionary after remodification : ",westeros,"\n")
 
 #if you want to view just the keys
-print(westeros.keys())
+print("Print dictionary keys : ",westeros.keys(),"\n")
 
 #if you want to view just the values
-print(westeros.values())
+print("Print dictionary values : ",westeros.values(),"\n")
 
 #print the value by specifying key
-print(westeros['cersei'])
+print("Print dictionary value by key : ",westeros['cersei'],"\n")
 
 #if you want to delete by its key name
 del westeros['cersei']
 #print it the updated dictionary
-print(westeros)
+print("Dictionary after deleting  : ",westeros,"\n")
 
 #reinitialize the dictionary
 westeros={'Syed':'Kotwal'}
-print(westeros)
+print("Dictionary values after reinitializing : ",westeros,"\n")
 del westeros['Syed']
-print("Dictionary after deleting key --> ",westeros)
+print("Dictionary after deleting key --> ",westeros,", Dictionary Length : ", westeros.__len__())
 
 #CHECK THE TYPE OF OBJECT
-print("Check type of object --> ",type(westeros))
+print("Check type of dictionary object --> ",type(westeros),"\n")
+
+customerdictionary = {'cust1':'ABC','cust2':'XYZ','cust3':'PQR'}
+
+print("Customers Keys : ", customerdictionary.keys(),"\n")
+print("Customers Values : ", customerdictionary.values(),"\n")
+print("Total Customers : ",customerdictionary.__len__(),"\n")

7168ccb308d69322e9056b8c4b0d4bfdd0b58cc5
syekotwa <syekotwa@publicisgroupe.net>
2018-09-05 15:33:18 +0530

    practise session on other_datatypes, sets, tuples, lists and String data type

--- BasicExamples/set.py
+++ BasicExamples/set.py
@@ -1,6 +1,7 @@
 #SET OPERATIONS
 x=set("Welcome to Python World")
-print(x)
+print("\n=================Set allows only unique values=================\n")
+print("\nDisplaying Set -->> ",x)
 
 #DEFINE SET DATA TYPE
 l = ['s','s','d']
@@ -13,7 +14,13 @@ PRINT A SET ( IT REMOVES A DUPLICATE CHARACTERS FROM A LIST
 SET IS AN UNORDERED COLLECTION WHICH HAS UNIQUE ELEMENT IN IT
 '''
 #PRINT SET
-print(g)
+print("\n Displaying set after converting from list -->> ",g)
 
 #PRINT LENGTH
-print(len(g))
+print("Set length -->> ",len(g))
+
+employees=["ram","kiran","raj","prem", "ram","kiran"]
+print("\nPrinting Employees -->> ",employees,", Employees Length : ",len(employees))
+print("\nConverting from list to set\n")
+employeeSet = set(employees)
+print("Displaying Set values after converting from list -->> ",employeeSet,", Employees Length : ",len(employeeSet))

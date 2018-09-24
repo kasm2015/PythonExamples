285bf05c724e8af35aa66d1063b586748767c6b0
unknown <kasm20@gmail.com>
2018-09-24 23:35:48 +0530

    add & substraction method implementation done

--- Functions_Examples/AddNumbers.py
+++ Functions_Examples/AddNumbers.py
@@ -6,9 +6,11 @@ def add(num1,num2):
     return num3
 
 result = add(10,30)
+print(add.__doc__)
 print("Addition of two numbers --> ",result)
 #retrieve the doc string for the function
-print(add.__doc__)
+print("************ function __doc__ describes about the method***********")
+
 
 #THIS IS FUNCTION TO SUBSTRACT TWO NUMBERS
 def sub(n1,n2):
@@ -17,5 +19,5 @@ def sub(n1,n2):
     return n3
 
 result  = sub(10,30)
-print("Substraction of two numbers --> ",result)
 print(sub.__doc__)
+print("Substraction of two numbers --> ",result)
